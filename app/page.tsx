e890877ce8070eaee6978641f99a4c28760220e7
lailas123 <163895596+lailas123@users.noreply.github.com>
2024-03-25 18:52:37 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
+      {allPosts
+  .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()) // Sort posts by date in ascending order
+  .map((post) => (
+     <article key={post._id}>
+      <Link href={post.slug}>
+        <h2>{post.title}</h2>
+      </Link>
+      {post.description && <p>{post.description}</p>}
+    </article>
+  ))}
     </div>
   )
 }
